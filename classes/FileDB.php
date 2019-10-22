3544eae48c999a10a6c359e097ed271702cb526b
KamileGuogyte <kamile.guogyte@gmail.com>
2019-10-22 12:09:22 +0300

    OOP pradžia

---
+++ classes/FileDB.php
@@ -0,0 +1,14 @@
+<?php
+
+
+class FileDB {
+    private $file_name;
+    
+    public function _construct($file_name) {
+        $this->file_name = $file_name; 
+        
+    }
+}
+
+//nieko nedaro svaime index.php mes jau sukuriam objekta
+
