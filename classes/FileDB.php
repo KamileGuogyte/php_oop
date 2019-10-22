5c08fe24ebbd57ba50ff38ff6bf3d5f62d6754e9
KamileGuogyte <kamile.guogyte@gmail.com>
2019-10-22 12:46:50 +0300

    OOP pradžia

--- classes/FileDB.php
+++ classes/FileDB.php
@@ -2,13 +2,22 @@
 
 
 class FileDB {
+
     private $file_name;
     
-    public function _construct($file_name) {
-        $this->file_name = $file_name; 
-        
+    /** @var $data array Duomenu masyvas */
+    private $data;
+
+    /**
+     * F-ija, kuri issikviecia sukurus objekta
+     * @param type $file_name Failo pavadinimas
+     */    
+    public function __construct($file_name) {
+        $this->file_name = $file_name;
     }
+
+    public function load() {
+        $this->data = file_to_array($this->file_name);
+    }
+
 }
-
-//nieko nedaro svaime index.php mes jau sukuriam objekta
-
