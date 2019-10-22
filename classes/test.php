088017753fbe46e82971aa21d47844974304f924
KamileGuogyte <kamile.guogyte@gmail.com>
2019-10-22 17:49:51 +0300

    initial commit

--- classes/test.php
+++ classes/test.php
@@ -2,27 +2,38 @@
 
 class Person {
     
-    public $ugis;
-    public $vardas;
-    public $asmens_kodas;
+   private $ugis;
+   private $vardas;
+   private $asmens_kodas;
+    //igaus verte tik kostruktoriui pasileidus
     
     public function __construct($vardas, $centimetrai) {
         $this->vardas = $vardas;
         $this->ugis = $centimetrai;
         $this->asmens_kodas = rand(1000, 9999);
         $this->kalbeti();
+       //konxtruktorius kalbeti iskviecia paskutini
     }
                 
       private function kalbeti() {
          print "labas, as $this->vardas,  mano ugis yra $this->ugis";
-      }          
+      }  
+      
+      public function pasakykVarda() {
+          return $this->vardas;
+          
+      }
+      
 }
 
+//objektas
 $zmogus1 = new Person ('Tadas', 186);
-var_dump($zmogus_1);
+var_dump($zmogus1);
+print $zmogus1->pasakykVarda(); 
 
+
+//objektas
 $zmogus2 = new Person ('Petras', 190);
-var_dump($zmogus_2);
-
-
+var_dump($zmogus2);
+print $zmogus2->pasakykVarda(); 
 
--- index.php
+++ index.php
@@ -1,6 +1,45 @@
 <?php
 
+
+require 'functions/file.php';
 require 'classes/FileDB.php';
 
-$db = new FileDB('file.txt');
+//objekto sukurimas
+$db = new FileDB('data/file.txt');
+$db->load();
 
+//i objekta  norim pridedi eilute
+$db->addRow('Vartotojui', ['name' => 'Ramune', 'surname' => 'Uogyte']);
+//add row kiekvieno refresho metu vykdysis
+$db->save();
+//jei atirasysim puslapi toks issisaugios array saugom i faila
+
+//paloadins kas gules faile
+$db ->load();
+ var_dump($db->getData());
+//rodys null be load
+
+
+
+ 
+ 
+//$db->load();
+//$db setData(['table'=> [['name']] 
+
+   // var_dump($db->getData());
+
+//$db->save();
+
+
+//$db->getRow('table', 0); 
+//table surado 0 eilute ir geazino aurimas stacenka
+
+
+//$db->load();
+//$db ->save(); //uzissevinins uzjasoninras
+
+//$db->
+
+//var_dump($db);
+
+//eiles tvarka cia yra svarbi
